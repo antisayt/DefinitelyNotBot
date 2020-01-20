645d77accbb2f22574c3c2d5f5fe698ddcbe1837
antisayt <49001471+antisayt@users.noreply.github.com>
2020-01-20 12:44:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,16 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    if (message.content === '!!info') {
+    	message.channel.send('Evo koje informacije treutno imam sto se tice kupovine overlay-a, banner-a, logotipa itd..
+
+Dostupni su u roku od 2 dana nakon kupovine
+
+Placanje je moguce jedino putem paypal i kreditnom karticom
+
+Placanje se vrsi prije dobivanja proizvoda
+
+Kupovinu izvrsi pisanjem komande !!kupovina');
   	}
 });
 
